13bd6d06a85b6d273f3469aaf9ab9ec0844f6c7a
HiroUrata <1712026@i.softbank.jp>
2021-08-13 17:41:10 +0900

    tableView　追加

--- TableViewLesson01/ViewController.swift
+++ TableViewLesson01/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let tableViewContentsArray = ["1","2","3","4","5","6","7","8","9","10"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        <#code#>
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        <#code#>
+    }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
 
